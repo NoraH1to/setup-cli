45fb7e9558a430e612c424d373be00aad6d971c3
NoraH1to <norah1to@qq.com>
2023-02-24 18:45:42 +0800

    fix(scripts/init): failed to initialize properly

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,18 @@
-import { execa } from 'execa';
+import { execaCommand } from 'execa';
+import chalk from 'chalk';
 
 try {
-  await execa('setup-cli update');
-} catch {
-  /* empty */
+  await execaCommand('setup-cli update', {
+    stdout: 'inherit',
+    stdin: 'inherit',
+    stderr: 'inherit',
+  });
+} catch (e) {
+  console.error(
+    chalk.black.bgYellow('NOTICE!'),
+    chalk.yellow(
+      'Init default repo fail. Please execute setup-cli update manually.',
+    ),
+  );
+  console.error(e.stack);
 }
