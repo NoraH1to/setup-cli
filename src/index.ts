58d6f5f85f0e8ba6c2ab74af6bbbfe807d826b2e
NoraH1to <norah1to@qq.com>
2023-03-03 16:59:11 +0800

    chore: add version command, format code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import { program, Argument } from 'commander';
 import cmdCreate from './command/create';
 import cmdUpdate from './command/update';
 import cmdRepo, { OPT } from './command/repo';
+import reset from './command/reset';
 import VError from 'verror';
+import { FileInfo } from './File';
+import { useEnvVarCurrent } from './utils';
+
+import type { Hooks as _Hooks } from './Hook';
+
+export type { InjectHook, BaseHook } from './Hook';
+export type Hooks = Partial<_Hooks>;
 
 const errHandle = (err: Error | VError) => {
   if (err instanceof VError) {
@@ -34,11 +42,18 @@ program
   .description('Manage repository')
   .action((opt) => cmdRepo({ opt }));
 
-program.command('reset').action(() => reset());
+program
+  .command('reset')
+  .description('Reset the CLI')
+  .action(() => reset());
+
+program.version(
+  new FileInfo({
+    pathname: path.resolve(
+      useEnvVarCurrent(import.meta.url).__dirname,
+      '../package.json',
+    ),
+  }).getJson().version as string,
+);
 
 program.parse();
-
-import type { Hooks as _Hooks } from './Hook';
-import reset from './command/reset';
-export type { InjectHook, BaseHook } from './Hook';
-export type Hooks = Partial<_Hooks>;
